d9a3b4c7f6da8bb92dc618ee80272d7e2c45cfc6
fiqy hidayat <fiqyjr25@gmail.com>
2021-01-31 03:03:16 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.json('API is work!'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('server running on PORT ${PORT}'));
